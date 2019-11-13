20c0657cf46208021264d546cfab85cb283298c7
james ross <james_ross@outlook.fr>
2019-11-13 19:43:59 +0100

    incorporated firebase, send not yet receive

--- src/landingPage.js
+++ src/landingPage.js
@@ -1,94 +1,50 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
+require('dotenv').config();
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  databaseURL: "https://message-app-2f7b3.firebaseio.com",
+  projectId: process.env.REACT_APP_FIREBASE_PROJEDT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: "1:318418595733:web:100410dda5ec1bc75c909c",
+  measurementId: "G-KH72VESE32"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+let messageRef = firebase.database().ref('messages');
 
 class LandingPage extends Component {
+
   constructor(props) {
-    // When you pass props to super, the props get assigned to 'this'
     super(props);
-    // this.state is the buildiing block of the data that gets used
+
     this.state = {
-      message: '',
       name: '',
-      list: []
+      message: '',
     }
   }
 
-  // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-
-  // component mounted means when component is injected into DOM tree
-  componentDidMount() {
-    //incorporating local storage so data survives a full browser restart
-    // note: session storage saved data survives just a page refresh
-    this.hydrateStateWithLocalStorage();
-    // add event listener to save state to localStorage
-    // when user leaves/refreshes the page 
-    window.addEventListener(
-      // "beforeunload" means before leaving the current page
-      "beforeunload",
-      this.saveStateToLocalStorage.bind(this)
-    );
+  changeHandler = e => {
+    this.setState({ [e.target.name]: e.target.value })
   }
+  submitHandler = e => {
+    e.preventDefault()
 
-  // unmount is a function called immediately before the component is ended/destroyed 
-  componentWillUnmount() {
-    window.removeEventListener(
-      "beforeunload",
-      this.saveStateToLocalStorage.bind(this)
-    );
-    // saves data to local storage if component successfully unmounts
-    this.saveStateToLocalStorage();
-  }
+    let name = this.state.name;
+    let text = this.state.message;
 
-  hydrateStateWithLocalStorage() {
-    // for loop through all items in state
-    for (let key in this.state) {
-      // if 'key' (our random id number) exists in localStorage
-      if (localStorage.hasOwnProperty(key)) {
-        // get the key's value from localStorage
-        let value = localStorage.getItem(key);
-
-        // parse the localStorage string (data saved) and setState
-        try {
-          value = JSON.parse(value);
-          this.setState({ [key]: value });
-        } catch (e) {
-          // handle empty string
-          this.setState({ [key]: value });
-        }
-      }
+    function saveMessage(name, text, id) {
+      let newMessageRef = messageRef.push();
+      newMessageRef.set({
+        name,
+        text,
+        id
+      })
     }
-  }
-
-  saveStateToLocalStorage() {
-    // for loop through every item in React state to rewrite our list
-    for (let key in this.state) {
-      // save to localStorage
-      localStorage.setItem(key, JSON.stringify(this.state[key]));
-    }
-  }
-  // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-
-  updateInput(key, value) {
-    // update react state with input data
-    this.setState({
-      [key]: value
-    })
-  }
-
-  sendMessage() {
-    const newMessage = {
-      id: 1 + Math.random(),
-      message: this.state.newMessage,
-      name: 'You'
-    }
-
-    const list = [...this.state.list];
-
-    list.push(newMessage);
-
-    this.setState({
-      list,
-      newMessage: ''
-    })
+    saveMessage(name, text, id);
   }
 
   render() {
@@ -103,26 +59,27 @@ class LandingPage extends Component {
       <div className='messagesDiv'>
         <ul>
           {/* List array is mapped through*/}
-          {this.state.list.map(item => {
+          {/* {this.state.list.map(item => {
             return (
               <li className={(item.name === 'You' ? 'right' : 'left')}
                 key={item.id}>
                 {item.name}: {item.message}
               </li>
             )
-          })}
+          })} */}
         </ul>
       </div>
 
       {/* message text area and send button here  */}
       <div className='inputDiv'>
-        <form>
-          <input type='text' className='input'
+        <form onSubmit={this.submitHandler}>
+          <input name="message"
+            placeholder="Send message..."
             value={this.message}
-            onChange={e => this.updateInput('newMessage', e.target.value)}
-          />
-          <button className='btn btn-primary'
-            onClick={() => this.sendMessage()}>Send</button>
+            onChange={this.changeHandler}
+            id='formText'
+            required />
+          <button className='submit' type="submit">Send Message</button>
         </form>
       </div>
 
@@ -130,7 +87,6 @@ class LandingPage extends Component {
       <button className='button nickname'>Nickname</button>
       <button className='button think'>Think...</button>
       <button className='button delete'>Delete</button>
-
     </div>
   }
 }
