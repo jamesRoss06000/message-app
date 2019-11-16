60ad1dad7a039732585d7b9fe455864165139f57
james ross <james_ross@outlook.fr>
2019-11-16 19:21:46 +0100

    Updates

---
+++ precache-manifest.dc1150d662cc1a3c4ce0332243a5852d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1afb73b335b162e3f24223d71d3d4f61",
+    "url": "/message-app/index.html"
+  },
+  {
+    "revision": "9c426808bbb0470221a1",
+    "url": "/message-app/static/css/main.2f6e5fa3.chunk.css"
+  },
+  {
+    "revision": "3c5121f4611dd2c8455c",
+    "url": "/message-app/static/js/2.cfc2752c.chunk.js"
+  },
+  {
+    "revision": "9c426808bbb0470221a1",
+    "url": "/message-app/static/js/main.5a0d37ab.chunk.js"
+  },
+  {
+    "revision": "cb5441ee520f6d374663",
+    "url": "/message-app/static/js/runtime-main.b4350b37.js"
+  }
+]);
